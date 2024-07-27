010439ff52269599ca54a936bdc1ef6dea41b5b5
Hatoon F. Aloqaily <115896985+AloqailyHat@users.noreply.github.com>
2024-07-27 20:08:37 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -220,9 +220,11 @@ if st.session_state['selected_option']:
         raghad_number = 966569566905
         hatoon_number = 966550275960
         musaab_number = 966500283891
+        hamad_number = 966550894355
         st.markdown(f"- [Raghad Almalki](https://www.linkedin.com/in/raghad-almalki-aa0aa71b2/) - {raghad_number}")
-        st.markdown(f"- [Mussab]() - {musaab_number} ")
+        st.markdown(f"- [Mussab](www.linkedin.com/in/musabalsobhi) - {musaab_number} ")
         st.markdown(f"- [Hatoon Aloqaily-](https://www.linkedin.com/in/hatoon-al-oqaily-73b808253/) - {hatoon_number}")
+        st.markdown(f"- [Hamad Altamimi-]() - {hamad_number}")
 
     if st.button("🔙"):
         st.session_state['selected_option'] = None
